4637cc6ae4672fbd1302706ca2f396b99a82e6ef
laveha <vdb12122005@gmail.com>
2024-11-06 09:30:07 +0300

    first commit

---
+++ app/src/main/java/com/example/menushka/BlankFragment.java
@@ -0,0 +1,16 @@
+package com.example.menushka;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.menushka.R;
+
+public class BlankFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/menushka/ThirdFragment.java
@@ -0,0 +1,17 @@
+package com.example.menushka;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.menushka.R;
+
+public class ThirdFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+}
